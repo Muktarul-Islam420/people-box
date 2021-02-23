7eedebd753d374c254d3d8c1ee4d00ce1da3352c
khan4200000 <www.muktarul420@gmail.com>
2021-02-23 10:49:12 +0600

    auto search with modal for people box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useRef, useState,  } from 'react';
+import './App.css'
+import CharacterCard from './components/CharacterCard/CharacterCard';
 
-function App() {
+const App = () => {
+  const [characterList, setCharacterList] = useState([]);
+  const [display, setDisplay] = useState(true);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+ 
+  const wrapperRef = useRef(null);
+  //Load data from api
+    useEffect(() =>{
+      axios
+      .get("https://rickandmortyapi.com/api/character/?name=rick&page=1")
+      .then(response =>{
+        console.log(response.data.results);
+        setCharacterList(response.data.results);
+        setOptions(response.data.results);
+      })
+      .catch(err =>{
+        console.error(err);
+      });
+    }, []);  
+  
+
+// Display or hide List with mousedown event
+    useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = event => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(true);
+    }
+  };
+
+  //update input with search
+    const updatePokeDex = poke => {
+    setSearch(poke);
+    setDisplay(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="character-lis">
+      <h1>Rick and Morty Search</h1>
+      <div className="input-box">
+      <input type="text" name="" placeholder="Search for contact" className="input"
+       onClick={() => setDisplay(!display)}
+       value={search}
+       onChange={event => setSearch(event.target.value)}
+      />
+      <i class="fas fa-search search-icon" aria-hidden="true"></i>
+      </div>
+      {
+        display || options &&  
+        characterList.filter(({ name }) => name.indexOf(search.toLowerCase()) > -1).map((chars,id) => {
+          return <CharacterCard onClick={() => updatePokeDex(chars.name)} chars={chars} key={id} tabIndex="0"></CharacterCard>
+        })
+      }
     </div>
   );
-}
+};
 
 export default App;
